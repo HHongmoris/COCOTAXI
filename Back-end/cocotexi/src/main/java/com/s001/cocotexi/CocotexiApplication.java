b1e06f175bb7dc634bc78efdfa324a5e792e4843
HHongmoris <moris8470@gmail.com>
2023-10-18 15:52:33 +0900

    [Feat] #S09P31S101-160 스프링부트 프로젝트 생성

---
+++ Back-end/cocotexi/src/main/java/com/s001/cocotexi/CocotexiApplication.java
@@ -0,0 +1,13 @@
+package com.s001.cocotexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocotexiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocotexiApplication.class, args);
+	}
+
+}
