8395229b1b5c84c11eac0a49ea5809479f34cc3b
mintae <alsxo824@naver.com>
2023-10-23 10:52:07 +0900

    [FEAT] OSM 적용 test

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,37 +1,109 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Polyline,
+  Marker,
+  Popup,
+} from "react-leaflet";
+import axios from "axios";
 
-// hi
+class RoutePlanner extends Component {
+  constructor() {
+    super();
+    this.state = {
+      startLocation: "",
+      endLocation: "",
+      route: null,
+      duration: null,
+    };
+  }
 
-function App() {
-  const [count, setCount] = useState(0)
+  handleStartLocationChange = (event) => {
+    this.setState({ startLocation: event.target.value });
+  };
 
-  return (
-    <>
+  handleEndLocationChange = (event) => {
+    this.setState({ endLocation: event.target.value });
+  };
+
+  calculateRoute = () => {
+    const { startLocation, endLocation } = this.state;
+
+    const apiKey = "5b3ce3597851110001cf624888240bdfef7d494bb8e36cbbd1683d77";
+    const apiUrl = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${apiKey}&start=${startLocation}&end=${endLocation}`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        const route = response.data.features[0].geometry.coordinates;
+        const duration =
+          response.data.features[0].properties.segments[0].duration;
+
+        this.setState({ route, duration });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  render() {
+    const { route } = this.state;
+    const routeCoordinates = route
+      ? route.map((coord) => [coord[1], coord[0]])
+      : [];
+
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          type="text"
+          placeholder="Start Location"
+          value={this.state.startLocation}
+          onChange={this.handleStartLocationChange}
+        />
+        <input
+          type="text"
+          placeholder="End Location"
+          value={this.state.endLocation}
+          onChange={this.handleEndLocationChange}
+        />
+        <button onClick={this.calculateRoute}>Calculate Route</button>
+
+        {route && (
+          <div>
+            <div>Estimated Duration: {this.state.duration} seconds</div>
+            <div>Route Coordinates: {JSON.stringify(route)}</div>
+          </div>
+        )}
+
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          style={{ height: "100vh", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            minZoom={0}
+            maxZoom={19}
+          />
+          {/* 경로를 표시하는 Polyline 추가 */}
+          <Polyline
+            pathOptions={{ color: "red", weight: 5 }}
+            positions={routeCoordinates}
+          />
+
+          {/* 마커 추가 */}
+          <Marker position={[51.5, -0.09]}>
+            <Popup>
+              A pretty CSS popup.
+              <br />
+              Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default RoutePlanner;
