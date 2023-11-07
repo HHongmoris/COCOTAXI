dd4637c1c3ea1b3366fb316882df9ff8825c8e94
HHongmoris <moris8470@gmail.com>
2023-11-07 14:01:37 +0900

    [FEAT] 운전자 위치 변경 로직 추가(현재 1번 드라이버만)

---
+++ Back-end/cocotaxi/src/main/java/com/s001/cocotaxi/service/MovingService.java
@@ -0,0 +1,32 @@
+package com.s001.cocotaxi.service;
+
+import com.s001.cocotaxi.domain.DriverMoving;
+import com.s001.cocotaxi.repository.MovingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MovingService {
+    private final MovingRepository movingRepository;
+    private int movingCount = 0; // 초기값 0
+
+    @Scheduled(fixedRate = 3000)    //1초(1000밀리초)
+    public List<Double> updateDriverLocation(int driverId){
+        // 0과 1을 번갈아가며 변경
+        movingCount = (movingCount == 0) ? 1 : 0;
+        DriverMoving driverMoving = movingRepository.findByDriverAndMovingCount(driverId, movingCount);
+
+        // 변경된 위치 정보 리스트를 반환합니다.
+        List<Double> locationList = new ArrayList<>();
+        locationList.add(driverMoving.getDriverLatitude());
+        locationList.add(driverMoving.getDriverLongitude());
+
+        // 위치 정보 리스트를 반환합니다.
+        return locationList;
+    }
+}
